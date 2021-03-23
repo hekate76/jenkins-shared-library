5cb157af3067c846e3475c69151de43b39ccd724
johnny <johnny@mitake.com.tw>
2021-03-23 17:30:37 +0800

    2

--- vars/template2.groovy
+++ vars/template2.groovy
@@ -1,11 +1,4 @@
 def call(Map params) {
-	def subModules = []
-	if ("${params.submodules}" != null) {
-		for (int i = 0; i < params.submodules.size(); i++) {
-			subModules.add("[$class: 'SubmoduleOption', disableSubmodules: false, parentCredentials: true, recursiveSubmodules: false, trackingSubmodules: false, reference: 'https://gitlab01.mitake.com.tw/apptech/nomura-library-gateway.git']")
-		}
-	}
-	
 	node {
 		try{
 			stage('Git Clone'){
@@ -13,7 +6,7 @@ def call(Map params) {
 				checkout([
 					$class: 'GitSCM', 
 					branches: [[name: "${params.branch}"]], 
-					extensions: subModules,
+					extensions: "${params.subModules}",
 					userRemoteConfigs: [[credentialsId: 'ssh', url: "${params.repositoryUrl}"]]
 				])
 				echo 'Clone End'
